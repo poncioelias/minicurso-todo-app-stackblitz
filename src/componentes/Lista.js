32f27bb53837c60b2045c22e7d8694d96df7767e
Ronie <roniemeque@outlook.com>
2018-11-01 05:47:29 -0300

    step 5

--- src/componentes/Lista.js
+++ src/componentes/Lista.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Tarefa from './Tarefa';
 
-const Lista = (props) => {
+class Lista extends Component {
+  state= {
+    filtro: ''
+  }
 
-  console.log(props);
+  handleInput = e => {
+    //esse evento tem um currentTarget
+    //dentro do currentTarget temos o value
+    const value = e.currentTarget.value;
+    this.setState({
+      filtro: value
+    })
+  }
 
-  const tarefas = props.tarefas;
-  const toggleConcluida = props.toggleConcluida;
-  const toggleArquivada = props.toggleArquivada;
+  render() {
+    //usando destructuring
+    const {filtro} = this.state;
+    const {tarefas, toggleArquivada, toggleConcluida} = this.props;
 
-  return (
-    <ul className="lista-tarefas">
-      {!!tarefas.length 
-        ? tarefas.map(tarefa => 
-          <Tarefa tarefa={tarefa} toggleConcluida={toggleConcluida} toggleArquivada={toggleArquivada} />
-        ) 
-        : <li>Sem tarefas</li>}
-    </ul>
-  );
+    //ao inves de usar os lowerCase eh possivel usar regex
+    const tarefasFiltradas = filtro ? tarefas.filter(({descricao}) => descricao.toLowerCase().includes(filtro.toLowerCase())) : tarefas;
+
+    return (
+      <ul className="lista-tarefas">
+        <input type="text" className="input input-busca" value={filtro} placeholder="Filtrar" onChange={this.handleInput} />
+        {!!tarefasFiltradas.length 
+          ? tarefasFiltradas.map(tarefa => 
+            <Tarefa tarefa={tarefa} toggleConcluida={toggleConcluida} toggleArquivada={toggleArquivada} />
+          ) 
+          : <li>Sem tarefas</li>}
+      </ul>
+    );
+  }
 }
 
 Lista.propTypes = {
--- src/componentes/Tarefa.js
+++ src/componentes/Tarefa.js
@@ -11,8 +11,8 @@ const Tarefa = (props) => {
     <li key={tarefa.id} className="tarefa">
       <span className="tarefa__descricao">{tarefa.descricao}</span>
       <div className="tarefa__botoes">
-        <button className="button" onClick={() => toggleConcluida(tarefa.id)}>{tarefa.completa ? 'Não concluída 🧐' : 'Feito ✅'}</button>
-        <button className="button" onClick={() => toggleArquivada(tarefa.id)}>🔥</button>
+        <button className="button" onClick={() => toggleConcluida(tarefa.id)}>{tarefa.completa ? 'Desconcluir 🧐' : 'Concluir ✅'}</button>
+        <button className="button" onClick={() => toggleArquivada(tarefa.id)}>{tarefa.arquivada ? 'Restaurar 🤙' : 'Arquivar 🔥'}</button>
       </div>
     </li>
   );
